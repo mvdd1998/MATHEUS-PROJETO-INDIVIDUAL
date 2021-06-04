3cd69277eb8abdcd9dc69c2e4df39fa0758c12bf
Matheus Vieck <matheus.mvdd@gmail.com>
2021-06-04 15:36:52 -0300

    Atualização 04/06

--- Banco de dados/dropnike.sql
+++ Banco de dados/dropnike.sql
@@ -2,15 +2,23 @@ create database dropnike;
 
 use dropnike;
 
-
-create table usuario (
+create table usuario(
 idusuario int primary key auto_increment,
 nome varchar (100),
-email varchar (100),
-size char (2),
+login varchar (100),
+size int,
 senha varchar (45)
-)auto_increment = 1001;
+)auto_increment = 100;
 
 select * from usuario;
 
-insert into usuario values (null, 'matheus', 'matheus.vieck@hotmail.com', '41', '1234');
+select avg(size) from usuario;
+
+create table comentar(
+idcomentar int primary key auto_increment,
+comentario varchar (150),
+fk_usuario int,
+foreign key (fk_usuario) references usuario(idusuario)
+) auto_increment = 1;
+
+select * from comentar;
