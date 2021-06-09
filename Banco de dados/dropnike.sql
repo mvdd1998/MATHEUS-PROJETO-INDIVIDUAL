2c4ddcaa4d11c63d5d52fc59f87dd403262637e3
unknown <matheus.mvdd@gmail.com>
2021-06-09 19:21:26 -0300

    finalizado

--- Banco de dados/dropnike.sql
+++ Banco de dados/dropnike.sql
@@ -26,9 +26,27 @@ select * from publicacao;
 
 select * from usuario;
 
-select avg(size) from usuario;
+select  round(avg(size),0)as Media, max(size) as Maior, min(size) as Menor  from usuario;
 
+select nome, size from usuario;
 
+select * from usuario join publicacao on publicacao.fkusuario = usuario.id;
+
+-- AIR FORCE 1 OFF WHITE
+select usuario.id, usuario.nome, usuario.size, publicacao.descricao, publicacao.fklancamento from usuario 
+join publicacao on publicacao.fkusuario = usuario.id where fklancamento = 1;
+
+-- DUNK SB WHAT THE PAUL
+select usuario.id, usuario.nome, usuario.size, publicacao.descricao, publicacao.fklancamento from usuario 
+join publicacao on publicacao.fkusuario = usuario.id where fklancamento = 2;
+
+-- AIR JORDAN 1 MID COLORS
+select usuario.id, usuario.nome, usuario.size, publicacao.descricao, publicacao.fklancamento from usuario 
+join publicacao on publicacao.fkusuario = usuario.id where fklancamento = 3;
+
+-- DUNK SCRAP
+select usuario.id, usuario.nome, usuario.size, publicacao.descricao, publicacao.fklancamento from usuario 
+join publicacao on publicacao.fkusuario = usuario.id where fklancamento = 4;
 
 
 
